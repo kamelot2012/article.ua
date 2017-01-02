77e14b609a4c1127d5ffba2795ad56a15731dd6f
kamelot2012 <kamelot2012@mail.ru>
2017-01-02 14:29:48 +0200

    02.01.2017 - 2

---
+++ views/news/insert.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+<form action="/index.php?ctrl=Admin&act=One" method="post">
+    title <input type="text" name="title">
+    <br>
+    content <textarea name="content"></textarea>
+    <br>
+    <input type="submit">
+</form>
+</body>
+</html>
